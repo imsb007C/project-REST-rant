4b518c2017812ea5f8d93742b03d2d52aa5cbdd4
Alex <imsb005@gmail.com>
2022-07-11 22:42:19 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,31 @@
 const router = require('express').Router()
 
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai-food.jpg'
+},{
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+}]
+router.get('/', (req, res) => {
+    
+    res.render('places/index',{places})
+})
+
+router.post('/', (req, res) =>{
+    places.push(req.body)
+    res.redirect('/places')
+})
 
 router.get('/new',(req,res)=>{
     res.render('places/new')
 })
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpg'
-    },{
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-    }]
-    res.render('places/index',{places})
-})
 
 module.exports = router
